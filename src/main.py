a3191589919a42cbf4f01636a518e40afbda5800
curogihu <curoasg@gmail.com>
2022-06-10 16:49:29 +0900

    add to fetch kill log about killing big moster

--- src/main.py
+++ src/main.py
@@ -22,6 +22,71 @@ def value_check():
   # return mo.fetch_game_duration
   pass
 
+@app.get('/log/big-monster-kill')
+def fetch_big_monster_kill():
+  base_game_overview_path = os.path.join("D:", os.sep, "output", "game", "timeline", "NA1_*.json")
+
+  file_paths = glob(base_game_overview_path)
+  big_monster_kill_log = []
+
+  tmp = []
+
+  killer_logs = []
+
+  for file_path in file_paths[:3]:
+    json_data = None
+
+    # print('file_path: ', file_path)
+
+    match_id = os.path.splitext(os.path.basename(file_path))[0]
+
+    with open(file_path , 'r') as json_file:
+      json_data = json.load(json_file)
+      killer_log = []
+
+      for frame in json_data.get('info').get('frames'):
+        if not frame.get('events'):
+          continue
+
+        for event in frame.get('events'):
+          # print('event: ', event.get('type'))
+        
+          tmp.append(event.get('type'))
+
+          if event.get('type') != 'ELITE_MONSTER_KILL':
+            continue
+
+          if event.get('monsterType') == 'DRAGON':
+            killer_team_id = event.get('killerTeamId')
+            killed_boss_name = event.get('monsterSubType')
+            elapsed_match_duration = event.get('timestamp') // 1000
+
+          else:
+            killer_team_id = event.get('killerTeamId')
+            killed_boss_name = event.get('monsterType')
+            elapsed_match_duration = int(event.get('timestamp') // 1000)
+
+            # killer team id will be 300 in case of that herald may be killed by itself.
+            if killer_team_id not in [100, 200]:
+              continue
+
+          killer_log.append(
+            {
+              elapsed_match_duration: {
+                # 'match_id': os.path.splitext(os.path.basename(file_path))[0],
+                'killer_team_id': killer_team_id,
+                'killed_boss_name': killed_boss_name,
+              }  
+            }
+          )
+
+    killer_logs.append(
+      {
+        match_id: killer_log
+      }
+    )
+
+  return killer_logs
 
 @app.get('/game-duration')
 def extract_game_duration():
